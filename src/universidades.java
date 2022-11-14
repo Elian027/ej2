a55264d4f7c7001f6760b1dcee8bb78ef7011611
Clarks1223 <pablo.uchuari@epn.edu.ec>
2022-11-14 10:50:07 -0500

    Se crea el constructor con sus respectivos Getter y Setter

--- src/universidades.java
+++ src/universidades.java
@@ -4,4 +4,42 @@ public class universidades {
     int n_edificios;
     int n_carreras;
 
+    public universidades(){
+        nombre = "National Politechnic School";
+        n_maestros=1000;
+        n_edificios=26;
+        n_carreras=50;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getN_maestros() {
+        return n_maestros;
+    }
+
+    public void setN_maestros(int n_maestros) {
+        this.n_maestros = n_maestros;
+    }
+
+    public int getN_edificios() {
+        return n_edificios;
+    }
+
+    public void setN_edificios(int n_edificios) {
+        this.n_edificios = n_edificios;
+    }
+
+    public int getN_carreras() {
+        return n_carreras;
+    }
+
+    public void setN_carreras(int n_carreras) {
+        this.n_carreras = n_carreras;
+    }
 }
